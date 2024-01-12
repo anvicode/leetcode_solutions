b3e826a0f695a4b322d237637dc5b45dcecf9a99
Andrey Vorontsov <reygofor@gmail.com>
2024-01-12 12:10:15 +0300

    add "9-palindrome-number.py second solution"

--- Python/9-palindrome-number.py
+++ Python/9-palindrome-number.py
@@ -4,3 +4,14 @@ class Solution:
             return False
         xs = str(x)
         return True if xs == xs[::-1] else False
+
+    def isPalindrome2(self, x: int) -> bool:
+        if x < 0:
+            return False
+        buff = x
+        rn = 0
+        while buff != 0:
+            d = buff % 10
+            rn = rn * 10 + d
+            buff //= 10
+        return rn == x
