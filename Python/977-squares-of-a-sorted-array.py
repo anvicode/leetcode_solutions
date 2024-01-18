bded93459792cbec724a428e0771e2f2c9fab462
Andrey Vorontsov <reygofor@gmail.com>
2024-01-18 11:09:44 +0300

    add "977-squares-of-a-sorted-array.py third solution"

--- Python/977-squares-of-a-sorted-array.py
+++ Python/977-squares-of-a-sorted-array.py
@@ -7,3 +7,18 @@ class Solution:
 
     def sortedSquares2(self, nums: List[int]) -> List[int]:
         return sorted(map(lambda x: x * x, nums))
+
+    def sortedSquares3(self, nums: List[int]) -> List[int]:
+        res = []
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            left = nums[l] * nums[l]
+            right = nums[r] * nums[r]
+            if left > right:
+                res.append(left)
+                l += 1
+            else:
+                res.append(right)
+                r -= 1
+        res.reverse()
+        return res
