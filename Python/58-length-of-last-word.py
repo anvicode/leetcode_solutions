8d6fa583daf042da8a36fc4742d11ec7c5ae13aa
Andrey Vorontsov <reygofor@gmail.com>
2024-01-20 11:45:44 +0300

    add "58-length-of-last-word.py second solution"

--- Python/58-length-of-last-word.py
+++ Python/58-length-of-last-word.py
@@ -1,3 +1,13 @@
 class Solution:
-    def lengthOfLastWord(self, s):
+    def lengthOfLastWord(self, s: str) -> int:
         return len(s.split()[-1])
+
+    def lengthOfLastWord2(self, s: str) -> int:
+        c = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == " ":
+                if c > 0:
+                    return c
+            else:
+                c += 1
+        return c
