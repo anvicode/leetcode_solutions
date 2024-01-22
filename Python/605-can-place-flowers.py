8df1798f81d5a5e79d4794038a08056a3d158300
Andrey Vorontsov <reygofor@gmail.com>
2024-01-22 15:17:19 +0300

    add "605-can-place-flowers.py second solution"

--- Python/605-can-place-flowers.py
+++ Python/605-can-place-flowers.py
@@ -21,3 +21,11 @@ class Solution:
                 n -= 1
                 flowerbed[i] = 1
         return n < 1
+
+    def canPlaceFlowers2(self, flowerbed: List[int], n: int) -> bool:
+        fb = [0] + flowerbed + [0]
+        for i in range(1, len(fb) - 1):
+            if fb[i - 1] == 0 and fb[i] == 0 and fb[i + 1] == 0:
+                n -= 1
+                fb[i] = 1
+        return n < 1
