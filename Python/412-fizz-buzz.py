5c6322e514ff8adde7858b66dd80d2d7bcb28e2a
Andrey Vorontsov <reygofor@gmail.com>
2024-02-16 18:41:26 +0300

    add "412-fizz-buzz.py second solution"

--- Python/412-fizz-buzz.py
+++ Python/412-fizz-buzz.py
@@ -11,3 +11,16 @@ class Solution:
             else:
                 answer[i] = str(i + 1)
         return answer
+
+    def fizzBuzz2(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append("FizzBuzz")
+            elif i % 3 == 0:
+                answer.append("Fizz")
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return answer
